25a906eb2d9657dc2bf2aaf1a1a8891002f205df
Valentin-69 <valentin@cvbhome.be>
2017-03-21 12:06:21 +0100

    reset the request file, it didn't work anymore

--- src/client/Request.java
+++ src/client/Request.java
@@ -12,11 +12,36 @@ public class Request {
 			giveIllegalArgument("Not enough arguments");
 			throw new IllegalArgumentException();
 		}
-		command = extractCommand(args);		                        // Set the command
-		String[] uriHostAndFile = extractUriHostAndFile(args); 		// setting the host and file
-		uriHost = uriHostAndFile[0];
-		uriFile = uriHostAndFile[1];
-		port = extractPort(args);		                            // setting the port
+		
+		command = HTTPCommands.getType(args[0]);
+		if(command==null){
+			giveIllegalArgument("Wrong command");
+			throw new IllegalArgumentException();
+		}
+		
+		if(args[1].contains("://")){
+			args[1]=args[1].substring(args[1].indexOf("://")+3, args[1].length());
+		}
+		
+		int index= args[1].indexOf("/",args[1].indexOf("."));
+		if(index!=-1){ //there is a / after a point in the string
+			uriHost = args[1].substring(0, index);
+			uriFile = args[1].substring(index, args[1].length());
+		}else{
+			uriHost = args[1];
+			uriFile = "/";
+		}
+
+		try{
+			if(args.length==2){
+				port=80;
+			}else{
+				port = Integer.parseInt(args[2]);
+			}
+		}catch(NumberFormatException e){
+			giveIllegalArgument("port has to be a number");
+			throw new IllegalArgumentException();
+		}
 	}
 	
 	public HTTPCommands getCommand(){
@@ -33,65 +58,6 @@ public class Request {
 	
 	public int getPort(){
 		return this.port;
-	}
-	
-
-	/**
-	 * Extracts the portnumber from the given args which consist of 2 or 3 elements.
-	 * @param args
-	 * 			[command, uri, port]  port is optional.
-	 */
-	private int extractPort(String[] args) {
-		int result;
-		try{
-			if(args.length==2){
-				result = 80;
-			}else{
-				result = Integer.parseInt(args[2]);
-			}
-		}catch(NumberFormatException e){
-			giveIllegalArgument("port has to be a number");
-			throw new IllegalArgumentException();
-		}
-		return result;
-	}
-
-	/**
-	 * Extracts the uriHost and uriFile from the given args which consist of 2 or 3 elements.
-	 * @param args
-	 * 			[command, uri, port]  port is optional.
-	 */
-	private String[] extractUriHostAndFile(String[] args) {
-		if(args[1].contains("://")){
-			args[1]=args[1].substring(args[1].indexOf("://")+3, args[1].length()); // for dealing with the http:// of the URI.
-		}
-		String[] result = new String[2];
-		int index= args[1].indexOf("/",args[1].indexOf("."));
-		String host, file;
-		if(index!=-1){ //there is a / after a point in the string
-			host = args[1].substring(0, index);
-			file = args[1].substring(index, args[1].length());
-		}else{
-			host = args[1];
-			file = "/";
-		}
-		result[0] = host;
-		result[1] = file;
-		return result;
-	}
-
-	/**
-	 * Extracts the command from the given args which consist of 2 or 3 elements.
-	 * @param args
-	 * 			[command, uri, port]  port is optional.
-	 */
-	private HTTPCommands extractCommand(String[] args) {
-		HTTPCommands result = HTTPCommands.getType(args[0]);
-		if(command==null){
-			giveIllegalArgument("Wrong command");
-			throw new IllegalArgumentException();
-		}
-		return result;
 	}
 	
 	public void execute(){
